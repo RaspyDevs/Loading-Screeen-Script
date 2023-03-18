53791b04128718629a31f0e2c203e2522119a8a4
RaspyDevs <128196962+RaspyDevs@users.noreply.github.com>
2023-03-17 20:50:33 -0400

    Add files via upload

---
+++ mp4_loadingscreen-main/fxmanifest.lua
@@ -0,0 +1,16 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Mailar#6308'
+description 'mp4_loadingscreen'
+version '1.0.0'
+
+loadscreen 'ui/index.html'
+
+files {
+    'ui/index.html',
+    'ui/style.css',
+    'ui/default.mp4',
+}
+
